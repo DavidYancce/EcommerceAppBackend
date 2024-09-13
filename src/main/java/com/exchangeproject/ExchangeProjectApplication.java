de9bc7711f3ec3b9d9dc0eed6646cd1d3dcbdb4b
David Yanccehuallpa <david.yanccehuallpa.q@uni.pe>
2024-09-12 20:00:01 -0500

    inital commit

---
+++ src/main/java/com/exchangeproject/ExchangeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.exchangeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeProjectApplication.class, args);
+	}
+
+}
