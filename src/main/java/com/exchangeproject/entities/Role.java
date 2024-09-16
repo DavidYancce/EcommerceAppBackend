52823098775f9382f2b35cccdd60aeb6c4df89a1
David Yanccehuallpa <david.yanccehuallpa.q@uni.pe>
2024-09-15 20:22:56 -0500

    fix: configs

--- src/main/java/com/exchangeproject/entities/Role.java
+++ src/main/java/com/exchangeproject/entities/Role.java
@@ -1,23 +1,17 @@
 package com.exchangeproject.entities;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Setter;
 
 @Entity
-@Table(name = "APP_USER")
+@Table(name = "APP_ROLE")
 @Data
-public class User {
+public class Role {
     @Id
     @GeneratedValue
     private Long id;
-    private String email;
-    private String password;
-    private String fullName;
-    public User() {}
+    private String name;
 }
