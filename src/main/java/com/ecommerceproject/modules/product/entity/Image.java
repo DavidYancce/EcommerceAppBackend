50a6dbb4aeaf81c5acd4efcbe9e9aa9c8ecbc86d
Marcelo Chambi <marcelo.chambi.22@gmail.com>
2024-10-17 21:59:44 -0500

    Feature: Image resources

---
+++ src/main/java/com/ecommerceproject/modules/product/entity/Image.java
@@ -0,0 +1,38 @@
+package com.ecommerceproject.modules.product.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TB_IMAGE", schema = "EXCHANGE_APP")
+@Data
+public class Image {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+    
+    @Column(unique = true, nullable = false)
+    private String url;
+
+    @Column(name = "is_main", nullable = false)
+    private Boolean isMain;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
