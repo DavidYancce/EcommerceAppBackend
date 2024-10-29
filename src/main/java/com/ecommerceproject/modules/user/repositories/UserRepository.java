f49e2493f0182a5a32c588c81f7570e56f2243d2
David Yanccehuallpa <david.yanccehuallpa.q@uni.pe>
2024-10-28 23:31:19 -0500

    feature:load permission in user

--- src/main/java/com/ecommerceproject/modules/user/repositories/UserRepository.java
+++ src/main/java/com/ecommerceproject/modules/user/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package com.ecommerceproject.modules.user.repositories;
 
 import com.ecommerceproject.modules.user.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +12,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+
+    @EntityGraph(attributePaths = {"role", "role.rolePermissions", "role.rolePermissions.permission"})
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmailWithPermissions(@Param("email") String email);
 }
